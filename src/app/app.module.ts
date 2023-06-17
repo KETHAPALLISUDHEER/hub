727fb4ce96130410684d6e7612cabe89f3f86669
shree <shreekrishnaprajapati5@gmail.com>
2023-06-17 11:41:50 +0530

    navbar-completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './MyComponent/navbar/navbar.component';
+import { RegisterComponent } from './MyComponent/register/register.component';
+import { LoginComponent } from './MyComponent/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
