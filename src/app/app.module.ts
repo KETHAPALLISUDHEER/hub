78ba80278259cc5f886097b80d5caf6938502d15
KETHAPALLISUDHEER <Kethapallisudheer14@gmail.com>
2023-06-20 20:57:09 +0530

    kss committed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,17 +10,12 @@ import { IdeasComponent } from './MyComponent/ideas/ideas.component';
 import { HomeComponent } from './MyComponent/home/home.component';
 import { MainhomeComponent } from './MyComponent/mainhome/mainhome.component';
 import { PostIdeasComponent } from './MyComponent/post-ideas/post-ideas.component';
-
 import { FooterComponent } from './MyComponent/footer/footer.component';
 import { ShreeComponent } from './MyComponent/shree/shree.component';
-
 import { SudheerComponent } from './MyComponent/sudheer/sudheer.component';
-
 import { ProjectComponent } from './MyComponent/project/project.component';
-
 import { AboutComponent } from './MyComponent/about/about.component';
 import { NavbarAboutComponent } from './MyComponent/navbar-about/navbar-about.component';
-
 import { FaqComponent } from './MyComponent/faq/faq.component';
 
 
@@ -36,20 +31,13 @@ import { FaqComponent } from './MyComponent/faq/faq.component';
     HomeComponent,
     MainhomeComponent,
     PostIdeasComponent,
-
     SudheerComponent,
     AboutComponent,
     NavbarAboutComponent,
     FooterComponent,
-
     FooterComponent,
     ShreeComponent,
-
-    
-    ProjectComponent,
-
-
-    
+    ProjectComponent, 
       FaqComponent,
 
     
